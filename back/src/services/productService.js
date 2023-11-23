53dce0f6b08bed8287690a32bf3b86a80f677a8b
[AI9] 장재웅 <wodndrhdqn@gmail.com>
2023-11-23 13:00:07 +0900

    Feat: service에 promise 적용

--- back/src/services/productService.js
+++ back/src/services/productService.js
@@ -2,17 +2,21 @@ var db = require("../db/db.js");
 console.log("productService 111111111111111");
 class productService {
   static async getProducts() {
-    // 시험용으로 user 테이블 조회하게 했습니다!
-    db.query("SELECT * FROM user", function (error, results, fields) {
-      if (error) throw error;
-      if (results.length > 0) {
-        console.log("productService 333333333333333333");
-        console.log("productService results값 확인 == ", results);
-        return results;
-      } else {
-        return `<script type="text/javascript">alert("상품이 없습니다."); 
-        document.location.href="/auth/login";</script>`;
-      }
+    return new Promise((resolve, reject) => {
+      // 시험용으로 user 테이블 조회하게 했습니다!
+      db.query("SELECT * FROM user", function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.length > 0) {
+            console.log("productService 333333333333333333");
+            console.log("productService results값 확인 == ", results);
+            resolve(results);
+          } else {
+            reject(new Error("상품이 없습니다."));
+          }
+        }
+      });
     });
   }
   //   static async getProducts() {
