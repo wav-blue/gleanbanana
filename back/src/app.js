d67374d50fb17c59f96a01d40293345ed2d2e9af
박수진 <wav0120@gmail.com>
2023-11-25 01:50:06 +0900

    Feat: db connect 구조 확정 및 쓰지않는 코드 제거

--- back/src/app.js
+++ back/src/app.js
@@ -4,12 +4,7 @@ import path from "path";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 // router import
-import { userRouter } from "./routers/userRouter";
 import { itemRouter } from "./routers/itemRouter";
-
-// db/index.js를 통해 db 연결
-// import { dbconnect } from "./db";
-// dbconnect();
 
 const app = express();
 
@@ -30,12 +25,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.send("기본 페이지");
 });
-console.log("app.js - app.get");
-app.use(userRouter);
-console.log("app.js - app.use(userRouter)");
+
 app.use(itemRouter);
-console.log("app.js - app.use(itemRouter)");
+
 // 에러 핸들링
 app.use(errorMiddleware);
-console.log("app.js - app.use(errorMiddleware)");
 export { app };
