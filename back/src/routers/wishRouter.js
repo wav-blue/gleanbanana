d8ef4c4457814dac3eb605f8e58fc15ebe2d987a
박수진 <wav0120@gmail.com>
2023-11-28 23:51:12 +0900

    Feat: Wishlist API 조회, 삭제 추가/스키마 변경

--- back/src/routers/wishRouter.js
+++ back/src/routers/wishRouter.js
@@ -6,19 +6,8 @@ const wishRouter = Router();
 // 찜한 목록 조회
 wishRouter.get("/user/:id/wishlist", async function (req, res, next) {
   try {
-    const items = await wishService.searchItems({ search });
-    res.status(200).json(items);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// 찜하기 리스트 생성
-wishRouter.post("/user/:id/wishlist/list", async function (req, res, next) {
-  const { id } = req.params;
-  console.log("id : ", id);
-  try {
-    const items = await wishService.createWishlist({ user_id: id });
+    const { id } = req.params;
+    const items = await wishService.getWishlist({ user_id: id });
     res.status(200).json(items);
   } catch (error) {
     next(error);
@@ -31,18 +20,24 @@ wishRouter.post("/user/:id/wishlist", async function (req, res, next) {
   const { item_id } = req.body; // item_id를 어떻게 받을까
 
   try {
-    const items = await wishService.createWishItem({ user_id: id, item_id });
+    const items = await wishService.createWishlist({
+      user_id: id,
+      item_id,
+    });
     res.status(200).json(items);
   } catch (error) {
     next(error);
   }
 });
 
-//찜한 목록 삭제
+//찜한 특정 아이템 삭제
 wishRouter.delete(
   "/user/:id/wishlist/:itemId",
   async function (req, res, next) {
     try {
+      const { id, itemId } = req.params;
+      await wishService.deleteWishitem({ user_id: id, item_id: itemId });
+      res.status(204).json();
     } catch (error) {
       next(error);
     }
@@ -52,6 +47,9 @@ wishRouter.delete(
 //찜한 목록 전체 삭제
 wishRouter.delete("/user/:id/wishlist", async function (req, res, next) {
   try {
+    const { id } = req.params;
+    await wishService.deleteAllWishitem({ user_id: id });
+    res.status(204).json();
   } catch (error) {
     next(error);
   }
--- back/src/services/wishService.js
+++ back/src/services/wishService.js
@@ -2,11 +2,11 @@ import db from "../db";
 import { v4 as uuidv4 } from "uuid";
 
 class wishService {
-  static async getUserWishlist({ user_id }) {
-    //
+  static async getWishlist({ user_id }) {
     return new Promise((resolve, reject) => {
-      const query = `SELECT wishlist_id FROM `;
-      db.query(query, function (error, results, fields) {
+      const query = `SELECT * FROM wishlist WHERE user_id = ?;`;
+
+      db.query(query, user_id, function (error, results, fields) {
         if (error) {
           reject(error);
         } else {
@@ -16,10 +16,9 @@ class wishService {
     });
   }
 
-  static async createWishItem({ user_id, item_id }) {
+  static async createWishlist({ user_id, item_id }) {
     return new Promise((resolve, reject) => {
-      const query = `INSERT INTO wishlist_item VALUES (null, (SELECT wishlist_id FROM wishlist WHERE user_id = ?), ?);
-      `;
+      const query = `INSERT INTO wishlist VALUES (null, ?, ?);`;
       db.query(query, [user_id, item_id], function (error, results, fields) {
         if (error) {
           reject(error);
@@ -29,32 +28,11 @@ class wishService {
       });
     });
   }
-  // 회원가입 시 위시리스트 생성
-  // 또는 위시리스트가 없다면?
-  static async createWishlist({ user_id }) {
+  // 특정 유저의 전체 아이템 삭제
+  static async deleteWishitem({ user_id, item_id }) {
     return new Promise((resolve, reject) => {
-      const query = `INSERT INTO wishlist_item VALUES (?, ?)`;
-      const wishlist_id = uuidv4();
-      db.query(
-        query,
-        [wishlist_id, user_id],
-        function (error, results, fields) {
-          if (error) {
-            reject(error);
-          } else {
-            console.log(wishlist_id, user_id);
-            resolve(results);
-          }
-        }
-      );
-    });
-  }
-
-  static async getWishlist() {
-    return new Promise((resolve, reject) => {
-      const query = ``;
-
-      db.query(query, function (error, results, fields) {
+      const query = `DELETE FROM wishlist WHERE user_id = ?  AND item_id = ?;`;
+      db.query(query, [user_id, item_id], function (error, results, fields) {
         if (error) {
           reject(error);
         } else {
@@ -63,7 +41,18 @@ class wishService {
       });
     });
   }
-  static async deleteWish() {}
-  static async deleteAllWish() {}
+  // 특정 유저의 특정 아이템 삭제
+  static async deleteAllWishitem({ user_id }) {
+    return new Promise((resolve, reject) => {
+      const query = `DELETE FROM wishlist WHERE user_id = ?;`;
+      db.query(query, user_id, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
 }
 export { wishService };
