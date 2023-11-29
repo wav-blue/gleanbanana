51ac86b01c00122cbc3fe09590e485e622f9c768
[AI9] 허제인 <codingjamee@gmail.com>
2023-11-29 16:33:10 +0900

    Merge: feature/back에서 가져온 back코드와 proxy설정을 위한 약간의 수정

---
+++ back/src/routers/itemRouter.js
@@ -0,0 +1,111 @@
+import { Router } from "express";
+
+import { itemService } from "../services/itemService";
+const itemRouter = Router();
+
+// 전체조회 + 카테고리 + 검색 조회
+itemRouter.get("/items", async function (req, res, next) {
+  const { category, search } = req.query;
+  try {
+    if (search) {
+      // 검색어가 제공된 경우 검색 수행
+      const items = await itemService.searchItems({ search });
+      res.status(200).json(items);
+    } else if (category) {
+      // 카테고리가 제공된 경우 카테고리별 조회
+      const items = await itemService.getItemsByCategory({ category });
+      res.status(200).json(items);
+    } else {
+      // 검색어와 카테고리가 모두 제공되지 않은 경우 전체 상품 조회
+      const items = await itemService.getItems({});
+      res.status(200).json(items);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 개별 조회
+itemRouter.get("/items/:itemId", async function (req, res, next) {
+  const { itemId } = req.params;
+  try {
+    const items = await itemService.getItem({
+      itemId,
+    });
+    res.status(200).json(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 추가
+itemRouter.post("/items", async function (req, res, next) {
+  try {
+    const {
+      item_id,
+      item_name,
+      category,
+      price,
+      description,
+      banana_index,
+      image_url,
+    } = req.body;
+
+    // DB에 데이터 추가
+    const newItems = await itemService.createItem({
+      item_id,
+      item_name,
+      category,
+      price,
+      description,
+      banana_index,
+      image_url,
+    });
+
+    if (newItems.errorMessage) {
+      throw new Error(newItems.errorMessage);
+    }
+    res.status(201).json(newItems);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 삭제
+itemRouter.delete("/items/:itemId", async function (req, res, next) {
+  try {
+    const { itemId } = req.params;
+    const items = await itemService.deleteItem({ item_id: itemId });
+    res.status(204).json(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 수정
+itemRouter.post("/items/:itemId", async function (req, res, next) {
+  try {
+    const { itemId } = req.params;
+    const { item_name, category, price, description, banana_index, image_url } =
+      req.body;
+
+    // DB에 데이터 수정
+    const updatedItem = await itemService.updateItem(itemId, {
+      item_name,
+      category,
+      price,
+      description,
+      banana_index,
+      image_url,
+    });
+
+    if (updatedItem.errorMessage) {
+      throw new Error(updatedItem.errorMessage);
+    }
+    res.status(200).json(updatedItem);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export { itemRouter };
---
+++ back/src/services/itemService.js
@@ -0,0 +1,132 @@
+import db from "../db";
+const table_name = "item";
+
+class itemService {
+  // 전체 조회
+  static async getItems() {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM ${table_name}`;
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.length > 0) {
+            console.log("getItems results값 확인 == ", results);
+            resolve(results);
+          } else {
+            reject(new Error("상품이 없습니다."));
+          }
+        }
+      });
+    });
+  }
+  // 개별 조회
+  static async getItem({ itemId }) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM ${table_name} WHERE item_id = ${itemId}`;
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.length > 0) {
+            console.log("getItem results값 확인 == ", results);
+            resolve(results);
+          } else {
+            reject(new Error("상품이 없습니다."));
+          }
+        }
+      });
+    });
+  }
+  // 추가
+  static async createItem({
+    item_id,
+    item_name,
+    category,
+    price,
+    description,
+    banana_index,
+    image_url,
+  }) {
+    var query = `INSERT INTO item (item_id,item_name,category,price,description,banana_index,image_url) VALUES (${item_id}, '${item_name}','${category}',${price},'${description}',${banana_index},'${image_url}')`;
+    return new Promise((resolve, reject) => {
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+  // 수정
+  static async updateItem(
+    item_id,
+    { item_name, category, price, description, banana_index, image_url }
+  ) {
+    return new Promise((resolve, reject) => {
+      const query = `UPDATE ${table_name} SET item_name='${item_name}',category='${category}',price='${price}',description='${description}',banana_index='${banana_index}',image_url='${image_url}' WHERE item_id = ${item_id};`;
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+  // 삭제
+  static async deleteItem({ item_id }) {
+    return new Promise((resolve, reject) => {
+      const query = `DELETE FROM ${table_name} WHERE item_id = ${item_id}`;
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  }
+
+  // 카테고리별 상품 조회
+  static async getItemsByCategory({ category }) {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM ${table_name} WHERE category = '${category}'`;
+
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.length > 0) {
+            resolve(results);
+          } else {
+            reject(new Error("해당 카테고리에 상품이 없습니다."));
+          }
+        }
+      });
+    });
+  }
+
+  // 검색 조회
+  static async searchItems({ search }) {
+    return new Promise((resolve, reject) => {
+      // 검색어를 이용한 SQL 쿼리 작성
+      const query = `SELECT * FROM ${table_name} WHERE item_name LIKE '%${search}%'`;
+
+      db.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          if (results.length > 0) {
+            console.log("searchItems 결과:", results);
+            resolve(results);
+          } else {
+            reject(new Error("검색 결과가 없습니다."));
+          }
+        }
+      });
+    });
+  }
+}
+export { itemService };
