b1c8cefce8da52e2e0579d449761ce9d245deb70
박수진 <wav0120@gmail.com>
2023-12-01 15:46:44 +0900

    Feat: Wishlist API 수정사항 반영

--- back/src/routers/wishRouter.js
+++ back/src/routers/wishRouter.js
@@ -4,10 +4,10 @@ import { wishService } from "../services/wishService";
 const wishRouter = Router();
 
 // 찜한 목록 조회
-wishRouter.get("/user/:id/wishlist", async function (req, res, next) {
+wishRouter.get("/:userId/wishlist", async function (req, res, next) {
   try {
-    const { id } = req.params;
-    const items = await wishService.getWishlist({ user_id: id });
+    const { userId } = req.params;
+    const items = await wishService.getWishlist({ user_id: userId });
     res.status(200).json(items);
   } catch (error) {
     next(error);
@@ -15,13 +15,13 @@ wishRouter.get("/user/:id/wishlist", async function (req, res, next) {
 });
 
 // 찜하기 버튼 클릭
-wishRouter.post("/user/:id/wishlist", async function (req, res, next) {
-  const { id } = req.params; // user의 id => 통해서 wishlist_id를 찾아냄?
-  const { item_id } = req.body; // item_id를 어떻게 받을까
+wishRouter.post("/:userId/wishlist", async function (req, res, next) {
+  const { userId } = req.params;
+  const { item_id } = req.body;
 
   try {
     const items = await wishService.createWishlist({
-      user_id: id,
+      user_id: userId,
       item_id,
     });
     res.status(200).json(items);
@@ -31,24 +31,21 @@ wishRouter.post("/user/:id/wishlist", async function (req, res, next) {
 });
 
 //찜한 특정 아이템 삭제
-wishRouter.delete(
-  "/user/:id/wishlist/:itemId",
-  async function (req, res, next) {
-    try {
-      const { id, itemId } = req.params;
-      await wishService.deleteWishitem({ user_id: id, item_id: itemId });
-      res.status(204).json();
-    } catch (error) {
-      next(error);
-    }
+wishRouter.delete("/:userId/wishlist/:itemId", async function (req, res, next) {
+  try {
+    const { userId, itemId } = req.params;
+    await wishService.deleteWishitem({ user_id: userId, item_id: itemId });
+    res.status(204).json();
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 //찜한 목록 전체 삭제
-wishRouter.delete("/user/:id/wishlist", async function (req, res, next) {
+wishRouter.delete("/:userId/wishlist", async function (req, res, next) {
   try {
-    const { id } = req.params;
-    await wishService.deleteAllWishitem({ user_id: id });
+    const { userId } = req.params;
+    await wishService.deleteAllWishitem({ user_id: userId });
     res.status(204).json();
   } catch (error) {
     next(error);
