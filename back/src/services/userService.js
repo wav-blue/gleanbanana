8a32745e6980b3099a120f70761c117d55e27ebc
박수진 <wav0120@gmail.com>
2023-12-07 21:54:05 +0900

    Refactor: User 리팩토링 작업 및 오류처리

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,27 +1,28 @@
 import bcrypt from "bcrypt";
-//import hashPassword from "../utils/hash-password";
 import { ulid } from "ulidx";
-import db from "../db";
-import { NotFoundError } from "../../libraries/custom-error";
+import { NotFoundError, ConflictError } from "../../libraries/custom-error";
 import { User } from "../db/DAO/User";
 import { createAccessToken, createRefreshToken } from "../utils/createToken";
 import { bcryptPassword } from "../utils/bcryptPassword";
 
 class userService {
-  static async addUser({ email, password, username, address, phone_number }) {
+  static async addUser({ email, password, ...userInfo }) {
     const user_id = ulid();
     const today = new Date();
 
+    // Email이 중복되는 경우
+    const conflict = await User.checkEmail({ email });
+    if (conflict) {
+      throw new ConflictError("이미 가입된 이메일입니다.");
+    }
     // password 암호화
     const encryptPassword = await bcryptPassword(password, 10);
 
     const newUser = {
       user_id,
+      ...userInfo, // username, address, phone_number
       email,
       password: encryptPassword,
-      username,
-      address,
-      phone_number,
       my_carbon: 0,
       createdAt: today,
       updatedAt: today,
@@ -29,10 +30,10 @@ class userService {
     };
 
     const results = await User.createUser({ newUser });
+
+    // 데이터베이스에 데이터 추가가 발생하지 않은 경우
     if (results.affectedRows === 0) {
-      return {
-        errorMessage: "회원가입 중 예기치 못한 에러가 발생했습니다.",
-      };
+      throw new Error("회원가입이 정상적으로 수행되지 않았습니다.");
     }
     return results;
   }
@@ -40,51 +41,42 @@ class userService {
   // 비밀번호 추가 작성 필요
   // 유저 로그인
   static async loginUser({ email, password }) {
-    try {
-      // 이메일로 유저 정보 조회
-      const findUser = await User.findUserByEmail({ email });
+    // 이메일로 유저 정보 조회
+    const findUser = await User.findUserByEmail({ email });
 
-      if (findUser.length === 0) {
-        return {
-          errorType: "NotFoundError",
-          errorMessage: "가입 이력이 없습니다.",
-        };
-      }
-
-      // 비밀번호 확인
-      const correctPasswordHash = findUser[0].password;
-      const isPasswordCorrect = await bcrypt.compare(
-        password,
-        correctPasswordHash
-      );
-
-      if (!isPasswordCorrect) {
-        return {
-          errorType: "UnauthorizedError",
-          errorMessage: "비밀번호가 일치하지 않습니다.",
-        };
-      }
-
-      // 로그인 성공 -> JWT 웹 토큰 생성
-      const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
-
-      const user_data = { user_id: findUser[0]["user_id"] };
-
-      // access token, refresh token 발급
-      const accessToken = await createAccessToken(user_data, secretKey);
-      const refreshToken = await createRefreshToken(secretKey);
-      // 반환할 loginuser 객체
-      const loginUser = {
-        accessToken,
-        refreshToken,
-        user_id: findUser[0]["user_id"],
-        errorMessage: null,
-      };
-
-      return loginUser;
-    } catch (error) {
-      return error;
+    if (findUser.length === 0) {
+      throw new NotFoundError("가입 이력이 없습니다.");
     }
+
+    // 비밀번호 확인
+    const correctPasswordHash = findUser[0].password;
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      correctPasswordHash
+    );
+
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedError("비밀번호가 일치하지 않습니다.");
+    }
+
+    // 로그인 성공 -> JWT 웹 토큰 생성
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
+
+    const user_data = { user_id: findUser[0]["user_id"] };
+
+    // access token, refresh token 발급
+    const accessToken = await createAccessToken(user_data, secretKey);
+    const refreshToken = await createRefreshToken(secretKey);
+
+    // 반환할 loginuser 객체
+    const loginUser = {
+      accessToken,
+      refreshToken,
+      user_id: findUser[0]["user_id"],
+      errorMessage: null,
+    };
+
+    return loginUser;
   }
 
   // 중복 이메일이 있는지 체크
@@ -96,65 +88,29 @@ class userService {
   // 유저 정보 조회
   static async getUser({ user_id }) {
     const findUser = await User.findUser({ user_id });
-
-    if (findUser?.length === 0) {
-      // id에 해당되는 유저가 없는 경우
-      return {
-        errorType: "NotFoundError",
-        errorMessage: "해당하는 유저를 찾을 수 없습니다.",
-      };
-    }
-    if (findUser[0]?.deletedAt) {
-      // 탈퇴한 유저인 경우
-      // 보안을 위해 같은 메시지 출력
-      return {
-        errorType: "NotFoundError",
-        errorMessage: "해당하는 유저를 찾을 수 없습니다.",
-      };
-    }
-
+    console.log("findUser >> ", findUser);
     return findUser;
   }
 
   // 유저 정보 수정
-  static async updateUser({
-    user_id,
-    password,
-    username,
-    address,
-    phone_number,
-  }) {
-    const updatedUser = {
-      password,
-      username,
-      address,
-      phone_number,
+  static async updateUser({ user_id, ...data }) {
+    const newData = {
+      ...data,
       updatedAt: new Date(),
     };
-    var query = `UPDATE user SET ? WHERE user_id = ?;`;
-    return new Promise((resolve, reject) => {
-      db.query(
-        query,
-        [updatedUser, user_id],
-        function (error, results, fields) {
-          if (error) {
-            console.log("error : ", error);
-            reject(error);
-          } else {
-            if (results.affectedRows === 0) {
-              reject(new NotFoundError("해당하는 유저를 찾을 수 없습니다."));
-            }
-            resolve(results);
-          }
-        }
-      );
-    });
+
+    const result = await User.updateUserInfo({ user_id, newData });
+    return result;
   }
   // 이미 회원탈퇴한 유저 확인하는 작업 필요
   // 회원 탈퇴
   static async deleteUser({ user_id }) {
-    const results = await User.updateDeletedAt({ user_id });
-    return results;
+    const checked = await User.checkDeletedAt({ user_id });
+    if (checked[0]["deletedAt"]) {
+      throw new ConflictError("이미 탈퇴한 유저입니다.");
+    }
+    const result = await User.updateDeletedAt({ user_id });
+    return result;
   }
 }
 export { userService };
