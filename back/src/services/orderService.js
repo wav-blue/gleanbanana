35f3a6b8e5c60eef2ae06a32e02d54e136639a39
박수진 <wav0120@gmail.com>
2023-12-08 16:51:08 +0900

    Refactor: Order 관련 수정사항 반영

--- back/src/services/orderService.js
+++ back/src/services/orderService.js
@@ -1,3 +1,4 @@
+import { NotFoundError } from "../../libraries/custom-error";
 import { Order } from "../db/DAO/Order";
 
 class orderService {
@@ -7,22 +8,26 @@ class orderService {
   }
   // 주문내역 상세조회
   static async getOrderDetail(userId, order_id) {
-    console.log("orderService.getOrderDetail(userId): ", userId);
-    console.log("orderService.getOrderDetail(order_id): ", order_id);
+    // 존재하는 order_id인지 확인
+    const results = await Order.checkOrderId({ order_id });
+    if (results.length <= 0) {
+      throw new NotFoundError("해당하는 주문 내역이 없습니다.");
+    }
     return Order.getOrderDetail(userId, order_id);
   }
 
   // 추가
   static async createOrder({ userId, pay_method, items }) {
-    console.log(" userId  ", userId);
-    console.log(" pay_method  ", pay_method);
-    console.log(" items  ", items);
     return Order.createOrder({ userId, pay_method, items });
   }
 
   //삭제
   static async deleteOrder(order_id) {
-    console.log("orderService.deleteOrder(order_id): ", order_id);
+    // 존재하는 order_id인지 확인
+    const results = await Order.checkOrderId({ order_id });
+    if (results.length <= 0) {
+      throw new NotFoundError("해당하는 주문 내역이 없습니다.");
+    }
     return Order.deleteOrder(order_id);
   }
 }
