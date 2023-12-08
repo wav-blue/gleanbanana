5e2a7e0363bf34786b2b69a7f43d1b4e18b4f136
박수진 <wav0120@gmail.com>
2023-12-08 12:43:40 +0900

    Feat: User, Wishlist, Orders 미들웨어 추가&&User 응답 수정 등

--- back/src/middlewares/loginRequired.js
+++ back/src/middlewares/loginRequired.js
@@ -15,17 +15,13 @@ function validateAccessToken(accessToken) {
 async function loginRequired(req, res, next) {
   try {
     const accessToken = req.signedCookies.accessToken ?? null;
-    console.log("loginRequired : ", accessToken);
     if (!accessToken) {
       throw new NotFoundError("Access token이 존재하지 않음");
     }
 
     // token 유효기간 검증
     const isAccessTokenValidate = validateAccessToken(accessToken);
-    console.log(
-      "loginRequired isAccessTokenValidate : ",
-      isAccessTokenValidate
-    );
+
     // Access Token 만료 => 재발급 요청
     if (!isAccessTokenValidate) {
       throw new TokenExpiredError("Access Token 만료");
--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -4,13 +4,13 @@ import {
   NotFoundError,
   UnauthorizedError,
 } from "../../libraries/custom-error";
+import jwt from "jsonwebtoken";
 import { userService } from "../services/userService";
 import { loginRequired } from "../middlewares/loginRequired";
-import { checkPermission } from "../middlewares/checkPermission";
+import { permission_check } from "../middlewares/permission_check";
 import { createAccessToken } from "../utils/createToken";
 import { validateRefreshToken } from "../utils/validateToken";
-
-import jwt from "jsonwebtoken";
+import { userId_check } from "../middlewares/userId_check";
 
 const userRouter = Router();
 
@@ -29,7 +29,6 @@ userRouter.post("/users/register", async function (req, res, next) {
       address,
       phone_number,
     });
-
     res.status(201).json("회원가입 완료");
   } catch (error) {
     next(error);
@@ -41,11 +40,12 @@ userRouter.get("/users/email", async function (req, res, next) {
   try {
     const { email } = req.query;
     const findEmail = await userService.getEmail({ email });
-    // "isDuplicated": false, "text": "중복되지 않은 이메일 주소
+
     const result = findEmail[0]["COUNT(email)"] ? true : false;
     const result_text = findEmail[0]["COUNT(email)"]
       ? "중복 된 이메일 주소"
       : "중복되지 않은 이메일 주소";
+
     res.status(200).json({
       isDuplicated: result,
       text: result_text,
@@ -60,9 +60,6 @@ userRouter.post("/users/login", async function (req, res, next) {
   try {
     const { email, password } = req.body;
     const user = await userService.loginUser({ email, password });
-
-    console.log("/users/login 발급된 accessToken >> ", user.accessToken);
-    console.log("/users/login 발급된 refreshToken >> ", user.refreshToken);
 
     res.cookie("accessToken", user.accessToken, {
       httpOnly: true,
@@ -129,90 +126,56 @@ userRouter.get("/current", loginRequired, async function (req, res, next) {
   }
 });
 
-// 유저 본인의 정보 조회
-userRouter.get("/:userId", async function (req, res, next) {
-  try {
-    //const user_id = req.currentUserId;
-    const { userId } = req.params;
-    const findUser = await userService.getUser({ user_id: userId });
-    if (findUser?.length === 0) {
-      // id에 해당되는 유저가 없는 경우
-      throw new NotFoundError("해당하는 유저를 찾을 수 없습니다.");
-    }
-    if (findUser[0]?.deletedAt) {
-      // 탈퇴한 유저인 경우: 보안을 위해 같은 메시지 출력
-      throw new NotFoundError("해당하는 유저를 찾을 수 없습니다(탈퇴한 유저).");
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// 유저 본인의 정보 조회
-userRouter.get(
-  "/:userId/login",
+// 회원 정보 수정
+userRouter.post(
+  "/users/:userId",
   loginRequired,
+  userId_check,
+  permission_check,
   async function (req, res, next) {
     try {
-      const user_id = req.currentUserId;
-      //const { userId } = req.params;
-      const user = await userService.getUser({ user_id });
-      res.status(200).json(user);
+      const userId = req.currentUserId;
+      const { username, address, phone_number } = req.body;
+      await userService.updateUser({
+        user_id: userId,
+        username,
+        address,
+        phone_number,
+      });
+      res.status(200).json("회원 정보 수정에 성공했습니다.");
     } catch (error) {
       next(error);
     }
   }
 );
 
-// 회원 정보 수정
-// userRouter.post("/users/:userId", async function (req, res, next) {
-//   try {
-//     const { userId } = req.params;
-//     const { password, username, address, phone_number } = req.body;
-//     const result = await userService.updateUser({
-//       user_id: userId,
-//       password,
-//       username,
-//       address,
-//       phone_number,
-//     });
-//     if (result.affectedRows === 0) {
-//       throw new NotFoundError("해당하는 유저를 찾을 수 없습니다.");
-//     }
-//     res.status(200).json("회원 정보 수정에 성공했습니다.");
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 // 로그아웃
-userRouter.get("/:userId/logout", async function (req, res, next) {
-  try {
-    // 토큰 파기
-    res.cookie("accessToken", null, {
-      maxAge: 0,
-    });
-    res.cookie("refreshToken", null, {
-      maxAge: 0,
-    });
-    res.send("로그아웃 완료");
-  } catch (error) {
-    next(error);
+userRouter.get(
+  "/:userId/logout",
+  loginRequired,
+  async function (req, res, next) {
+    try {
+      // 토큰 파기
+      res.cookie("accessToken", null, {
+        maxAge: 0,
+      });
+      res.send("로그아웃 완료");
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 // 회원탈퇴
 userRouter.delete(
   "/users/:userId",
   loginRequired,
-  checkPermission,
+  permission_check,
   async function (req, res, next) {
     try {
       const user_id = req.currentUserId;
-      const result = await userService.deleteUser({ user_id });
+      await userService.deleteUser({ user_id });
 
-      console.log("result: ", result);
       // 이미 탈퇴한 회원
       res.status(200).json("회원탈퇴가 완료되었습니다.");
     } catch (error) {
@@ -221,4 +184,36 @@ userRouter.delete(
   }
 );
 
+// User 세부 정보 조회
+userRouter.get("/myPage", loginRequired, async function (req, res, next) {
+  try {
+    const userId = req.currentUserId;
+    const result = await userService.getUseData({ user_id: userId });
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { userRouter };
+
+// // 유저 본인의 정보 조회
+// userRouter.get("/:userId", async function (req, res, next) {
+//   try {
+//     //const user_id = req.currentUserId;
+//     const { userId } = req.params;
+//     const findUser = await userService.getUser({ user_id: userId });
+//     if (findUser?.length === 0) {
+//       // id에 해당되는 유저가 없는 경우
+//       throw new NotFoundError("해당하는 유저를 찾을 수 없습니다.");
+//     }
+//     if (findUser[0]?.deletedAt) {
+//       // 탈퇴한 유저인 경우: 보안을 위해 같은 메시지 출력
+//       throw new NotFoundError("해당하는 유저를 찾을 수 없습니다(탈퇴한 유저).");
+//     }
+//     res.status(200).json(user);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
