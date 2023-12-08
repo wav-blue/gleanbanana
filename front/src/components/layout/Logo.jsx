38efbe1e72f266db51ad7be50776b4ca8522616b
iris2598 <iris2598@naver.com>
2023-12-09 02:19:19 +0900

    Style: logo 삽입

--- front/src/components/layout/Logo.jsx
+++ front/src/components/layout/Logo.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Logo = () => {
   return (
     <div className="logo__wrapper">
       <Link to="/">
-        <img src="" alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
       </Link>
     </div>
   );
